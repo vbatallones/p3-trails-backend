9636742e5b2021bf0b035051dcdb4eedbda7ee2f
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-09-07 23:30:52 -0700

    add display name and reference id in user schema

--- models/User.js
+++ models/User.js
@@ -1,24 +1,40 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now()
-    }
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	displayName: {
+		type: String,
+		default: 'hiker001',
+	},
+	// longitude: {
+	// 	type: Number,
+	// 	// required: true,
+	// },
+	// latitude: {
+	// 	type: Number,
+	// 	// required: true,
+	// },
+	// radiusTrail: {
+	// 	type: Number,
+	// 	// required: true,
+	// },
+	userTrails: [ {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Trail',
+        //5f56903a064669eff27e971c
+	} ]
 });
 
-module.exports = User = mongoose.model('User', UserSchema)
+module.exports = User = mongoose.model('User', UserSchema);
