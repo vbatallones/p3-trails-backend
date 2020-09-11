809a5ef300089df113f24cca1bd1f10f389d2bbb
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-09-10 18:32:18 -0700

    add route to delete the user favorites

--- routes/trails.js
+++ routes/trails.js
@@ -7,13 +7,13 @@ const Trail = require('../models/Trail');
 const User = require('../models/User');
 
 // Show/Detail
-router.get('/:id', (req, res) => {
-    db.Trail.findById(req.params.id)
-        .then((trails) => {
-            res.status(200).json(trails);
-        })
-        .catch((err) => res.status(500).json({ error: err }));
-});
+// router.get('/:id', (req, res) => {
+//     db.Trail.findById(req.params.id)
+//         .then((trails) => {
+//             res.status(200).json(trails);
+//         })
+//         .catch((err) => res.status(500).json({ error: err }));
+// });
 
 // Create
 // console.log(req.body)
@@ -56,19 +56,6 @@ router.post('/createtrail', (req, res) => {
             } else {
                 console.log("trail in userTrails")
             }
-            // console.log(newTrail)
-            // if(!newTrail){
-            //     // db.Trail.create({ name: req.body.name })
-            //     // .then(trail=>{
-            //         foundUser.userTrails.push(trail)
-            //         foundUser.save()
-            //     // })
-            //     // .catch(err => console.log("error1", err))
-            // }
-            // else{
-            //     // foundUser.userTrails.push(newTrail)
-            //     // foundUser.save()
-            // }
         })
         .catch(err => console.log("error2", err))
 
@@ -76,68 +63,32 @@ router.post('/createtrail', (req, res) => {
     .catch(err => console.log("error3", err))
 
 })
+router.post('/delete', (req, res) => {
+    console.log(req.body)
+    db.User.findById({ _id: req.body._id })
+    .then(user=>{
+        console.log(user)
+        let foundUser = user
+        db.User.findOne({ userTrails: {name: req.body.userTrails} })
+        // foundUser.userTrails.includes(req.body.name)
+        .then(newTrail=>{
+            let favedTrail = {name: req.body.userTrails}
+            if(newTrail){
+                console.log("deleting Trail")
+                foundUser.userTrails.remove(favedTrail)
+                foundUser.save()
+                console.log('--------',favedTrail)
+                res.send(user)
+                
+            } else {
+                console.log("trail in userTrails")
+            }
+        })
+        .catch(err => console.log("error2", err))
 
-// router.post('/create', (req, res)=>{
-//     db.Trail.findOne({ name: req.body.name })
-//   .then(trail => {
-//     // if email already exists, send a 400 response
-//     if (trail) {
-//         console.log("already in db")
-//         res.send(trail)
-//         // return res.status(204).json({ msg: 'trail found'});
-      
-//     } else {
-//       // Create a new user
-//       const newTrail = new Trail({
-//         name: req.body.name
-//       })
-//     //   db.User.userTrails.push(newTrail)
-//       console.log(db.User.userTrails)
-//       newTrail.save()
-//     }})
-//     .then(newTrail => {
-//         // console.log('this is the user🤯')
-//         // console.log("this is the body🥵", req.body)
-//         // console.log("new trail", newTrail)
-//         console.log(user)
-//         res.status(201).send(newTrail)
-//         db.User.findByIdAndUpdate(req.body.user.id, //req.body.currentUser
-//             {$addToSet: { userTrails: newTrail}},
-//             {safe: true}
-//         )
-//         .then((updated)=>res.send(updated))
-//         .catch(err=>res.send({ message: 'Error in adding newTrail to user', err}));
-    
-//     })
-//     .catch(err=>{
-//         console.log('Error while creating', err)
-//         if(err.name === 'ValidationError'){
-//             res.status(406).send({message: 'Validation Error'})
-//         } else {
-//             res.status(503).send({message:'Database or server error!'})
-//         }
-//     })
-//     // res.send('You\'ve hit the POST /bounties route!')
-// })
-///////////////////////////////////
+    })
+    .catch(err => console.log("error3", err))
 
-// router.put('/:id', (req, res)=>{
-//     db.User.findOneAndUpdate({
-//         _id: req.params.id
-//     },
-//     req.body,
-//     {
-//         new: true
-//     })
-//     .then(updatedUser=>{
-//         res.send(updatedUser)
-//     })
-//     .catch(err=>{
-//         console.log(err)
-//         res.status(503).send({message: 'Server Error'})
-//     })
-//     // res.send('You\'ve hit the PUT /bounties/:id route!')
-// })
-
+})
 
 module.exports = router;
